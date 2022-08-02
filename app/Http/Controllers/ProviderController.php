7c26852030111b8c078e4d13bb0f669f22dfc6be
Novi Sonko <novisonko@sonkotek.com>
2022-08-02 18:44:10 +0100

    Added business logic: provider fetch data

--- app/Http/Controllers/ProviderController.php
+++ app/Http/Controllers/ProviderController.php
@@ -13,55 +13,23 @@ class ProviderController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(App\Models\Provider $provider)
     {
-        $res= json_decode(file_get_contents('https://api.cqc.org.uk/public/v1/providers'), true)['providers'] ?? [];
+        $res= $provider->selectAll();
 
         return response()->json(['success' => true, 'data' => $res]);
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreProviderRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreProviderRequest $request)
-    {
-        //
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Provider  $provider
+     * @param string  $providerId
      * @return \Illuminate\Http\Response
      */
-    public function show(string $providerId)
+    public function show(\App\Models\Provider $provider, string $providerId)
     {
-        $res= [];
+        $res= $provider->selectByProviderId($providerId);
 
-        $saved= Provider::where('provider_id', '=', $providerId)->first();
-
-        if(!is_null($saved)){
-
-        } else {
-
-            $res= json_decode(file_get_contents('https://api.cqc.org.uk/public/v1/providers/' . $providerId), true);
-
-        }
-
-        return response()->json(['success' => true, 'data' => $res]);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Provider  $provider
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Provider $provider)
-    {
-        //
+        return response()->json(['data' => $res]);
     }
 }
--- app/Models/Provider.php
+++ app/Models/Provider.php
@@ -8,4 +8,58 @@ use Illuminate\Database\Eloquent\Model;
 class Provider extends Model
 {
     use HasFactory;
+
+    /**
+     * Get all records
+     * 
+     * @return array
+     */
+    public function selectAll()
+    {
+        return json_decode(file_get_contents('https://api.cqc.org.uk/public/v1/providers'), true)['providers'] ?? [];
+    }
+
+    /**
+     * Get a record
+     * 
+     * @param string $providerId
+     * 
+     * @return array
+     */
+    public function selectByProviderId(string $providerId)
+    {
+        $res= [];
+        // one week cache life
+        $cacheLife= 7 * 24 * 3600;
+
+        $saved= Provider::where('provider_id', '=', $providerId)->first();
+
+        if(!is_null($saved)){
+
+            // if cached data is too old, empty cache
+            if((time() - $saved['timestamp_updated']) > $cacheLife){
+
+                $saved= null;
+                Provider::where('provider_id', '=', $providerId)->delete();
+
+            }
+
+        } else {
+
+            $res= json_decode(file_get_contents('https://api.cqc.org.uk/public/v1/providers/' . $providerId), true);
+
+            if(!empty($res)){
+
+                $res['timestamp_updated']= time();
+
+                $provider= new Provider($res);
+                $provider->save();
+
+                $res= Provider::where('provider_id', '=', $providerId)->first();
+
+            }
+        }
+
+        return $res;
+    }
 }
