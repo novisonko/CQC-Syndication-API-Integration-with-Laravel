25ca68c18dfc6e88a81e74f173c379dec734ad31
Novi Sonko <novisonko@sonkotek.com>
2022-08-02 16:35:48 +0100

    Create providers table migration

--- database/migrations/2022_08_02_150737_create_providers_table.php
+++ database/migrations/2022_08_02_150737_create_providers_table.php
@@ -16,6 +16,33 @@ return new class extends Migration
         Schema::create('providers', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('providerId');
+            $table->string('locationIds');
+            $table->string('organisationType');
+            $table->string('ownershipType');
+            $table->string('type');
+            $table->string('name');
+            $table->string('brandId');
+            $table->string('brandName');
+            $table->string('registrationStatus');
+            $table->date('registrationDate');
+            $table->string('companiesHouseNumber');
+            $table->integer('charityNumber');
+            $table->string('website');
+            $table->string('postalAddressLine1');
+            $table->string('postalAddressLine2');
+            $table->string('postalAddressTownCity');
+            $table->string('postalAddressCounty');
+            $table->string('region');
+            $table->string('postalCode');
+            $table->integer('uprn');
+            $table->double('onspdLatitude');
+            $table->double('onspdLongitude');
+            $table->string('mainPhoneNumber');
+            $table->string('inspectionDirectorate');
+            $table->string('constituency');
+            $table->string('localAuthority');
+            $table->date('lastInspection');
         });
     }
 
